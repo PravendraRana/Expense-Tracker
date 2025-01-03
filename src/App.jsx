3fd23a465452b54e0715d03ae70eab17f43075a1
PravendraRana <pravendra56rana@gmail.com>
2025-01-03 21:06:04 +0530

    Expence tacker added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
+import { SnackbarProvider } from "notistack";
+import { ExpenseTracker, Transactions } from "./components";
+import { TransactionContext } from "./context/TransactionContext";
+import "./App.css";
 
 function App() {
+  const [recentTransactions, setRecentTransactions] = useState([]);
+  const [combinedExpenseData, setCombineExpenseData] = useState([]);
+
+  const value = {
+    recentTransactions,
+    setRecentTransactions,
+    combinedExpenseData,
+    setCombineExpenseData,
+  };
+
+  useEffect(() => {
+    
+    if (!localStorage.getItem("walletBalance")) {
+      localStorage.setItem("walletBalance", "5000");
+    }
+
+   
+    if (!localStorage.getItem("recentTransactions")) {
+      localStorage.setItem("recentTransactions", JSON.stringify([]));
+    }
+
+    if (!localStorage.getItem("expenses")) {
+      localStorage.setItem("expenses", "0");
+    }
+  }, []);
 
   return (
     <>
-      <h1>Expense Tracker</h1>
+      <TransactionContext.Provider value={value}>
+        <SnackbarProvider>
+          <div className="expense-paper">
+            <ExpenseTracker />
+            <Transactions />
+          </div>
+        </SnackbarProvider>
+      </TransactionContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/mocks/recentTransactions.mocks.js
@@ -0,0 +1,23 @@
+export const recentTransactions = [
+  {
+    id: "123",
+    category: "Food",
+    title: "Biryani",
+    date: "Jan 01,2024",
+    price: "250",
+  },
+  {
+    id: "473",
+    category: "Entertainment",
+    title: "Movie",
+    date: "Jan 02,2025",
+    price: "300",
+  },
+  {
+    id: "389",
+    category: "Travel",
+    title: "Uber",
+    date: "Jan 03,2025",
+    price: "150",
+  },
+];
